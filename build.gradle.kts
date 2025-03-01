2885dfd1bc2c0ff938a6fa5a9ccf19f99f522c23
Umaq <feratj01@gmail.com>
2025-03-01 17:19:25 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localization_Android"
+            url = uri("https://maven.pkg.github.com/umaq12/localization_android")
+            credentials {
+                username = "umaq12"
+                password = ""
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["kotlin"])
+            groupId = "com.github.umaq12"
+            version = "1.0.1"
+            artifactId = "localization_android"
+        }
+    }
+}
